e755458681f2a8e40541b3dc0cf8c885c9e738f8
preetish <pkrishnapritish@gmail.com>
2017-10-15 14:30:17 +0530

    Add files via upload

---
+++ rake.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 14 20:56:57 2017
+
+@author: preetish
+"""
+
+#RAKE(rapid Key word Extraction)
+'''implementation of Automatic keyword extraction from individual documents
+Stuart Rose, Dave Engel, Nick Cramer
+and Wendy Cowley'''
+
+#individual words without stopwords
+
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+import re
+
+
+Text=tri.lower()
+Text= re.sub('[^a-zA-Z0-9 ]','',Text)
+Text=''.join(Text)
+text=Text.split(' ')
+
+#remove excessive white spaces
+index=[]
+for i in range(len(text)):
+    if len(text[i])==0:
+        index.append(i)
+
+for i in range(len(index)):
+    index[i]-=i
+
+for i in range(len(index)):
+    text.pop(index[i])
+
+Text=' '.join(text)  
+remove=stopwords.words('english')
+text=[w for w in text if w not in remove]
+bow=CountVectorizer()
+text=' '.join(text)
+a=[text]
+freq=bow.fit_transform(a)
+
+freq1=freq.toarray()[0]
+freq2=bow.vocabulary_
+
+
+text1=Text
+
+for i in remove:
+    text1=re.sub(' '+i+' ' or ' '+i+'|' or '|'+i+' ' or '|'+i+'|',' | ',text1)
+    
+
+text1=text1.split('|')
+
+
+
+#compute degree    
+deg={}
+
+for i in text.split(' '):
+    if i not in list(deg.keys()):
+        deg[i]=0
+        for j in text1:
+            if i in j:
+                deg[i]+=1
+
+score={}
+
+key1=list(deg.keys())
+key2=list(freq2.keys())
+
+#stopped here 15/10/17
+for i in text.split(' '):
+    if i not in list(score.keys()) and i in key1 and i in key2 and freq1[freq2[i]]!=0:
+        score[i]=deg[i]/freq1[freq2[i]]
+
+
+scorefinal={}
+key=list(score.keys())
+
+for i in text1:
+    scorefinal[i]=0
+    used=[]
+    for j in text.split(' '):
+        if j in i and j not in used and j in key :
+            scorefinal[i]+=score[j]
+            used.append(j)
+
+
+
+#freq_based_dict
+fbd={}
+for i in scorefinal.keys():
+    fbd[str(scorefinal[i])]=i
+
+topten=sorted(fbd.keys())[::-1]
+topten=topten[:10]
+
+for i in topten:
+    print(fbd[i])
+
+
+#adjoining rare keywords
+
